eef14c794fa64177b4e43b37508d09d19748044a
Harshil Chauhan <Harshil@Chauhan.com>
2023-08-24 23:18:13 +0530

    User Registration Completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,61 +1,48 @@
-const asyncHandler = require("express-async-handler")
-const User = require("../models/User")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const db = require("../models/index");
+const jwt = require("jsonwebtoken");
 
 // @desc Get user information
 // @route GET /api/user
 // @access private
 const getUserInfo = asyncHandler(async (req, res) => {
-    res.status(200)
-    res.json({ message: "Get User Information successfully Called" })
-})
+    const data = db.User.findAll()
+  res.status(200);
+  res.json({ message: "Get User Information successfully Called", data});
+});
 
 // @desc Register a user
 // @route POST /api/user/register
 // @access public
 const userRegister = asyncHandler(async (req, res) => {
-    console.log(req?.body)
-    const { firstname, lastname, email, password, mobile, city } = req?.body
-    if (!firstname || !lastname || !email || !password || !mobile || !city) {
-        res.status(400)
-        throw new Error("All fields are mandatory")
-    }
-    const userAvailable = await User.findAll({
-        where: {
-            email: email
-        }
-    })
-    if (userAvailable) {
-        res.status(409)
-        throw new Error("Duplicate record found")
-    }
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const user = await User.create({
-        firstname,
-        lastname,
-        email,
-        password,
-        mobile,
-        city
-    })
-
-    console.log(user)
-    // res.status(200)
-    // res.json({messsage: "Get User Register successfully Called"})
-})
+  console.log(req?.body);
+  const { firstName, lastName, email, password, mobile } = req?.body;
+  if (!firstName || !lastName || !email || !password || !mobile) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await db.User.create({
+    firstName,
+    lastName,
+    email,
+    password: hashedPassword,
+    mobile,
+  });
+  res.status(200).json({ message: "User has been created successfully", user });
+});
 
 // @desc Register a user
-// @route POST /api/user/login 
+// @route POST /api/user/login
 // @access public
 const userLogin = asyncHandler(async (req, res) => {
-    res.status(200)
-    res.json({ message: "Get User Login successfully Called" })
-})
-
+  res.status(200);
+  res.json({ message: "Get User Login successfully Called" });
+});
 
 module.exports = {
-    getUserInfo,
-    userRegister,
-    userLogin
-}
+  getUserInfo,
+  userRegister,
+  userLogin,
+};
--- models/User.js
+++ models/User.js
@@ -1,40 +1,27 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require("../config/dbConnection")
-
-const User  = sequelize.define('User', {
-    firstname: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-
-    lastname: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-
-    accessToken: {
-        type: DataTypes.STRING
-    },
-
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-
-    mobile: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },  
-
-    city: {
-        type: DataTypes.STRING,
-        allowNull: false
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
     }
-})
-
-module.exports = {User}
+  }
+  User.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    mobile: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
