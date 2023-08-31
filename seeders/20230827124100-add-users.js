8ae80b20a184d5de91f009a3868d00e5cd89a5c6
Lucifer Morningstar <lucifer@morningstar.com>
2023-08-31 21:43:04 +0530

    with dotENv file

--- seeders/20230827124100-add-users.js
+++ seeders/20230827124100-add-users.js
@@ -1,30 +1,31 @@
 "use strict";
+const bcrypt = require("bcrypt")
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert(
-      "Device",
+      "users",
       [
         {
           firstName: "Harshil",
           lastName: "Chauhan",
           email: "harshil@chauhan.com",
-          password: "Harshil@123",
+          password: await bcrypt.hash("Harshil@123", 10),
           mobile: "7202845787",
         },
         {
           firstName: "Johnny ",
           lastName: "Depp",
           email: "johnny@depp.com",
-          password: "Harshil@123",
+          password: await bcrypt.hash("Harshil@123", 10),
           mobile: "8855774466",
         },
         {
           firstName: "Robert Downey ",
           lastName: "Jr.",
           email: "tony@stark.com",
-          password: "Harshil@123",
+          password: await bcrypt.hash("Harshil@123", 10),
           mobile: "6699558877",
         },
       ],
@@ -33,7 +34,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("Device", null, {});
+    await queryInterface.bulkDelete("users", null, {});
   },
 };
-s
